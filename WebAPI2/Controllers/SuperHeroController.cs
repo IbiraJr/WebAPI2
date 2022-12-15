a5642cbd196e82de6287f08c2fb9b9e22df42857
Ibira Junior <ibirajunior98@gmail.com>
2022-12-15 12:59:51 -0300

    first commit

---
+++ WebAPI2/Controllers/SuperHeroController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WebAPI2.Services.SuperHeroService;
+
+namespace WebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroService;
+
+        public SuperHeroController(ISuperHeroService superHeroService)
+        {
+           _superHeroService = superHeroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var result = _superHeroService.GetAllHeroes();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var result = _superHeroService.GetSingleHero(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero request)
+        {
+
+            var result = _superHeroService.AddHero(request);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var result = _superHeroService.UpdateHero(id, request);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var result = _superHeroService.DeleteHero(id);
+            
+            if (result is null)
+                return NotFound("Sorry.");
+
+            return Ok(result);
+        }
+    }
+}
+
